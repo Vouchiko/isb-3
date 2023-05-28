710260e47806d069fe26ab6a37d6e8b710388148
Vladimir <vovakarpov2017@gmail.com>
2023-05-28 18:33:12 +0400

    Added public_deserialization, private_deserialization, rsa_decription methods

--- decryption.py
+++ decryption.py
@@ -1,6 +1,8 @@
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives.asymmetric import padding as pd
+from cryptography.hazmat.primitives import hashes
 
 
 class Decrypt:
@@ -16,4 +18,21 @@ class Decrypt:
         cipher = Cipher(algorithms.ChaCha20(key, nonc), mode=None)
         decryptor = cipher.decryptor()
         dc_text = decryptor.update(text) + decryptor.finalize()
+        return dc_text
+
+    def public_deserialization(self, public_pem):
+        with open(public_pem, 'rb') as pem_in:
+            public_bytes = pem_in.read()
+        d_public_key = load_pem_public_key(public_bytes)
+        return d_public_key
+
+    def private_deserialization(self, private_pem):
+        with open(private_pem, 'rb') as pem_in:
+            private_bytes = pem_in.read()
+        d_private_key = load_pem_private_key(private_bytes, password=None, )
+        return d_private_key
+
+    def rsa_decription(self, c_text, private_key):
+        dc_text = private_key.decrypt(c_text, pd.OAEP(mgf=pd.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
+                                                      label=None))
         return dc_text
