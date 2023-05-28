ab2c1c1960db83ba38f92131e4acd1a4bdc31d0a
Vladimir <vovakarpov2017@gmail.com>
2023-05-28 17:40:56 +0400

    Added key_serialization and key_deserialization methods

--- encryption.py
+++ encryption.py
@@ -12,3 +12,12 @@ class Encrypt:
     def generation_nonce(self):
         n = os.urandom(16)
         return n
+
+    def key_serialization(self, patch, key):
+        with open(patch, 'wb') as key_file:
+            key_file.write(key)
+
+    def key_deserialization(self, patch, key):
+        with open(patch, mode='rb') as key_file:
+            content = key_file.read()
+        return content
