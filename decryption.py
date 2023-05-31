dc5007304fd901384ca5b101e79d9c4580c5be35
Vladimir <vovakarpov2017@gmail.com>
2023-05-31 12:23:18 +0400

    Added logging

--- decryption.py
+++ decryption.py
@@ -1,8 +1,11 @@
+import logging
+
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.primitives.asymmetric import padding as pd
 from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import rsa
 
 
 '''
@@ -14,20 +17,21 @@ class Decrypt:
     def __init__(self):
         pass
 
-    def un_padding_t(self, dc_text):
+    def un_padding_t(self, dc_text: bytes) -> str:
         """
-        Depadding of text by a symmetric algorithm
+        unpadding of text by a symmetric algorithm
 
             Parameters:
                dc_text: text for unpadding
             Return value:
                 unpadded_dc_text.decode(): The text with the unpadding algorithm applied to it
         """
+        logging.info('Unpadding of text by a symmetric algorithm...')
         unpadder = padding.ANSIX923(32).unpadder()
         unpadded_dc_text = unpadder.update(dc_text) + unpadder.finalize()
         return unpadded_dc_text.decode('UTF-8')
 
-    def simmetric_decryptor(self, text, key, inizializer, nonce):
+    def simmetric_decryptor(self, text: bytes, key: bytes, inizializer: bytes, nonce: bytes) -> bytes:
         """
         Decryption of text by symmetric algorithm
 
@@ -38,12 +42,13 @@ class Decrypt:
             Return value:
                 dc_text: The text with the decrypting algorithm applied to it
         """
+        logging.info('Decryption of text by symmetric algorithm...')
         cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None)
         decryptor = cipher.decryptor()
         dc_text = decryptor.update(text) + decryptor.finalize()
         return dc_text
 
-    def public_deserialization(self, public_pem):
+    def public_deserialization(self, public_pem: str) -> rsa.RSAPublicKey:
         """
         Deserializing the public key
 
@@ -52,12 +57,13 @@ class Decrypt:
             Return value:
                 d_public_key: deserialized public key
         """
+        logging.info('Deserializing the public key...')
         with open(public_pem, 'rb') as pem_in:
             public_bytes = pem_in.read()
         d_public_key = load_pem_public_key(public_bytes)
         return d_public_key
 
-    def private_deserialization(self, private_pem):
+    def private_deserialization(self, private_pem: str) -> rsa.RSAPrivateKey:
         """
         Deserializing the private key
 
@@ -66,12 +72,13 @@ class Decrypt:
             Return value:
                 d_private_key: deserialized private key
         """
+        logging.info('Deserializing the private key...')
         with open(private_pem, 'rb') as pem_in:
             private_bytes = pem_in.read()
         d_private_key = load_pem_private_key(private_bytes, password=None, )
         return d_private_key
 
-    def rsa_decryption(self, c_text, private_key):
+    def rsa_decryption(self, c_text: bytes, private_key: rsa.RSAPrivateKey) -> bytes:
         """
         Decryption of the text by an asymmetric algorithm
 
@@ -82,6 +89,7 @@ class Decrypt:
                 dc_text: decrypted text
 
         """
+        logging.info('Decryption of the text by an asymmetric algorithm...')
         dc_text = private_key.decrypt(c_text, pd.OAEP(mgf=pd.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
                                                       label=None))
         return dc_text
--- encryption.py
+++ encryption.py
@@ -1,3 +1,4 @@
+import logging
 import os
 from cryptography.hazmat.primitives import padding
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
@@ -15,73 +16,78 @@ class Encrypt:
     def __init__(self):
         pass
 
-    def generation_key(self):
-
+    def generation_key(self) -> bytes:
         """key generation"""
-
+        logging.info('Key generation...')
         key = os.urandom(32)
         return key
 
-    def generation_nonce(self):
-
+    def generation_nonce(self) -> bytes:
         """nonce generation"""
-
+        logging.info('Nonce generation...')
         n = os.urandom(16)
         return n
 
-    def key_serialization(self, patch, key):
+    def key_serialization(self, patch: str, key: bytes) -> None:
         """
-        serializing the symmetric algorithm key to a file
+       serializing the symmetric algorithm key to a file
 
             Parameters:
                 patch: file name
                 key: key
         """
+        logging.info('Serializing the symmetric algorithm key to a file...')
         with open(patch, 'wb') as key_file:
             key_file.write(key)
 
-    def key_deserialization(self, patch, key):
+    def key_deserialization(self, patch: str, key: bytes) -> bytes:
         """
-        deserialization of the symmetric algorithm key
+        Deserialization of the symmetric algorithm key
 
             Parameters:
                 patch: file name
+                key: key
+            Return value:
+                content: deserialized content
         """
+        logging.info('Deserialization of the symmetric algorithm key...')
         with open(patch, mode='rb') as key_file:
-            content = key_file.read()
+            content: bytes = key_file.read()
         return content
 
-    def padding_t(self, text):
+    def padding_t(self, text: str) -> bytes:
         """
-        data padding for block cipher operation
+        Data padding for block cipher operation
 
             Parameters:
-                text: text
+                text: text to be padded
             Return value:
                 padded_text: text with padding applied to it
         """
+        logging.info('Data padding for block cipher operation...')
         padders = padding.ANSIX923(32).padder()
         text = bytes(text, 'UTF-8')
-        padded_text = padders.update(text) + padders.finalize()
+        padded_text: bytes = padders.update(text) + padders.finalize()
         return padded_text
 
-    def simmetric_cryptor(self, text, key, inizializer, nonce):
+    def simmetric_cryptor(self, text: str, key: bytes, inizializer: bytes, nonce: bytes) -> bytes:
         """
-        data padding for block cipher operation
+        data encryption using symmetric algorithm
 
             Parameters:
                 text: text for encrypting
                 key: encryption key
-                nonce: Additional parameter
+                nonce: additional parameter
             Return value:
                 c_text: encrypted text
         """
+        logging.info('Data encryption using symmetric algorithm...')
         cipher = Cipher(algorithms.ChaCha20(key, nonce), mode=None)
         encryptor = cipher.encryptor()
-        c_text = encryptor.update(text) + encryptor.finalize()
+        c_text: bytes = encryptor.update(text) + encryptor.finalize()
         return c_text
 
-    def rsa_key_generate(self):
+    def rsa_key_generate(self) -> tuple:
         """
          generating a key pair for an asymmetric encryption algorithm
 
@@ -89,33 +95,36 @@ class Encrypt:
                 private_key
                 public_key
         """
+        logging.info('Generating a key pair for an asymmetric encryption algorithm...')
         keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
         private_key = keys
         public_key = keys.public_key()
         return private_key, public_key
 
-    def public_serialization(self, public_key):
+    def public_serialization(self, public_key) -> None:
         """
         serializing the public key to a file
             Parameters: public_key
         """
+        logging.info('Serializing the public key to a file...')
         public_pem = 'Data/keys/public.pem'
         with open(public_pem, 'wb') as public_out:
             public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
                                                      format=serialization.PublicFormat.SubjectPublicKeyInfo))
 
-    def private_serialization(self, private_key):
+    def private_serialization(self, private_key) -> None:
         """
         serializing the private key to a file
             Parameters: public_key
         """
+        logging.info('Serializing the private key to a file...')
         private_pem = 'Data/Encryption/private.pem'
         with open(private_pem, 'wb') as private_out:
             private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
                                                         format=serialization.PrivateFormat.TraditionalOpenSSL,
                                                         encryption_algorithm=serialization.NoEncryption()))
 
-    def rsa_encryption(self, text, public_key):
+    def rsa_encryption(self, text: str, public_key: str) -> bytes:
         """
          text encryption using RSA-OAEP
             Parameters:
@@ -124,6 +133,7 @@ class Encrypt:
             Return value:
                 c_text
         """
+        logging.info('Text encryption using RSA-OAEP...')
         c_text = public_key.encrypt(text,
                                     pd.OAEP(mgf=pd.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
                                             label=None))
